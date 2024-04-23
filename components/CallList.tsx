0ea0f88108039ff7d6f3d97c1a702bbff718afa1
Prince Effiong <prynzwill147@gmail.com>
2024-04-23 18:54:43 +0100

    Upcoming meeting page

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 'use client';
 import { useGetCalls } from '@/hooks/useGetCall';
 import { Call, CallRecording } from '@stream-io/video-react-sdk';
@@ -48,7 +49,39 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
   return (
     <div className='grid grid-cols-1 gap-5 xl:grid-cols-2'>
       {calls && calls.length > 0 ? (
-        calls.map((meeting: Call | CallRecording) => <MeetingCard />)
+        calls.map((meeting: Call | CallRecording) => (
+          <MeetingCard
+            key={(meeting as Call).id}
+            icon={
+              type === 'ended'
+                ? 'icons/upcoming.svg'
+                : type === 'upcoming'
+                ? 'icons/upcoming.svg'
+                : 'icons/recordings.svg'
+            }
+            title={
+              (meeting as Call).state.custom.description.substring(0, 22) ||
+              'No Description'
+            }
+            date={
+              (meeting as Call).state.startsAt?.toLocaleString() ||
+              meeting.start_time.toLocaleString()
+            }
+            isPreviousMeeting={type === 'ended'}
+            buttonIcon1={type === 'recordings' ? 'icons/play.svg' : undefined}
+            handleClick={
+              type === 'recordings'
+                ? () => router.push(`${meeting.url}`)
+                : () => router.push(`/meeting/${meeting.id}`)
+            }
+            link={
+              type === 'recordings'
+                ? meeting.url
+                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`
+            }
+            buttonText={type == 'recordings' ? 'Play' : 'Start'}
+          />
+        ))
       ) : (
         <h1>{noCallMessage}</h1>
       )}
