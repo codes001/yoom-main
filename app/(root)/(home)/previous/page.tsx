e5d55dd67ede96deb37a0e67075d5140eef0af8e
Prince Effiong <prynzwill147@gmail.com>
2024-04-24 11:08:40 +0100

    Previous meeting page

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,9 +1,11 @@
+import CallList from '@/components/CallList';
 import React from 'react';
 
 const Previous = () => {
   return (
     <section className='flex  text-white size-full flex-col gap-10'>
-      <h1 className='text-2xl font-bold'>Prev</h1>
+      <h1 className='text-2xl font-bold'>Previous Meetings</h1>
+      <CallList type='ended' />
     </section>
   );
 };
