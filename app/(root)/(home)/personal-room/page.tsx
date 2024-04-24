89e38e796df7dc01b9ebfe5ba6bee378288399b7
Prince Effiong <prynzwill147@gmail.com>
2024-04-24 11:35:25 +0100

    Added error handling to recordings page

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,9 +1,28 @@
+import { useUser } from '@clerk/nextjs';
 import React from 'react';
 
+interface TableType {
+  title: string;
+  description: string;
+}
+const Table = ({ title, description }: TableType) => (
+  <div>
+    <h1>{title}</h1>
+    <p>{description}</p>
+  </div>
+);
 const PersonalRoom = () => {
+  const { user } = useUser();
   return (
     <section className='flex  text-white size-full flex-col gap-10'>
-      <h1 className='text-2xl font-bold'>Personal</h1>
+      <h1 className='text-2xl font-bold'>Personal Meeting Room</h1>
+
+      <div className='flex w-full flex-col gap-5 xl:max-w-[900px]'>
+        <Table
+          title='Topic'
+          description={`${user?.username}'s Personal meeting room `}
+        />
+      </div>
     </section>
   );
 };
